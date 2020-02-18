91fa0eed94a73e4278f18d0eaabea8ba353a054e
Rachelle <rkozuma@hawaii.edu>
2020-02-18 10:16:07 -1000

    Completed parts 1-7

--- SwiftPlaygroundThree.playground/Contents.swift
+++ SwiftPlaygroundThree.playground/Contents.swift
@@ -1,3 +1,47 @@
 import UIKit
 
-var str = "Hello, playground"
+// Swift Playground #3: Introduction to Arrays
+
+// Part 2: Declare an Array
+
+//var fruitnames : [String] <-- Use only with new xcode
+
+// Part 3:
+
+var fruitnames = [ "Kiwi" , "Banana" , "Mango" ] // use var in this line for older version
+
+
+// Part 4:
+
+print(fruitnames)
+
+// Part 5:
+
+/*
+ Desired Result:
+ I like to eat Kiwi
+ I like to eat Banana
+ I like to eat Mango
+ */
+
+// print("I like to eat \(fruitnames)")
+
+//  Part 6: For In Loop
+
+for name in fruitnames
+{
+    print("I like to eat " + name + "'s.")
+}
+
+// Part 7:
+
+print(fruitnames[1])
+
+/*
+ fruitnames = [ "Kiwi" , "Banana" , "Mango"]
+ so,
+ Kiwi = 0
+ Banana = 1
+ Mango = 2
+ 
+ */
